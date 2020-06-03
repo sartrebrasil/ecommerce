9c6275cf477a8316fdd1f18025391578825db531
Sartre Brasil <sartre.brasil@oobj.com.br>
2020-06-03 18:04:34 -0300

    final project

--- commons-kafka/src/main/java/ecommerce/ConsumerFunction.java
+++
@@ -1,8 +0,0 @@
-package ecommerce;
-
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-
-public interface ConsumerFunction<T> {
-
-	void consume(ConsumerRecord<String, T> record) throws Exception;
-}
